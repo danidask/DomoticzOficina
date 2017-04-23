1b44a79959cb52e33785f3395cf585cfb3d71fc5
DasK <danidask@gmail.com>
2017-04-23 19:45:36 +0100

    Update and rename credentials (example).h to credentials(example).h

--- wemos_nodes/wemos_dht_json/credentials(example).h
+++ wemos_nodes/wemos_dht_json/credentials(example).h
@@ -1,4 +1,5 @@
 // Fill and rename to credentials.h
+// credentials.h is included in .gitignore, so you won't upload your password to github
 
 // Existing WiFi network
 const char* ssid     = "my_wifi";
